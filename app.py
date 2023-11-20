84bd97dbd07ebd3786925e1e211da66eb1ba0d28
emonggithub <paul.emong@gmail.com>
2023-11-20 13:14:46 +0300

    The file is updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+    
 from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
 from flask import Flask,request,render_template,jsonify
@@ -15,10 +16,10 @@ def home_page():
 def predict_datapoint():
     if request.method == "GET":
         return render_template("form.html")
-
+    
     else:
         data=CustomData(
-
+            
             carat=float(request.form.get('carat')),
             depth = float(request.form.get('depth')),
             table = float(request.form.get('table')),
@@ -29,16 +30,17 @@ def predict_datapoint():
             color= request.form.get('color'),
             clarity = request.form.get('clarity')
         )
-
+        # this is my final data
         final_data=data.get_data_as_dataframe()
-
+        
         predict_pipeline=PredictPipeline()
-
+        
         pred=predict_pipeline.predict(final_data)
-
+        
         result=round(pred[0],2)
-
+        
         return render_template("result.html",final_result=result)
 
+#execution begin
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
